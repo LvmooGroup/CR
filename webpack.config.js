e89de61e013eb76dd3de785101626d0acef966f8
LvmooGroup <kay2u@live.shop.edu.cn>
2016-12-24 12:50:42 +0800

    去掉注释plugins

--- webpack.config.js
+++ webpack.config.js
@@ -38,16 +38,16 @@ module.exports = {
             }
         ]
     },
-//     plugins: [
-//         new webpack.optimize.UglifyJsPlugin({
-//             compress: {
-//                 warnings: false
-//             }
-//         }),
-//         //new webpack.optimize.DedupePlugin(),
-//         new webpack.optimize.OccurenceOrderPlugin(),
-//         new webpack.DefinePlugin({
-//             'process.env.NODE_ENV': JSON.stringify('production')
-//         })
-//   ]
+     plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                 warnings: false
+             }
+         }),
+         //new webpack.optimize.DedupePlugin(),
+         new webpack.optimize.OccurenceOrderPlugin(),
+         new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production')
+         })
+   ]
 }
